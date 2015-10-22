2a0c64e30833b731094fb56ffafbd1ad01f98683
Nathawut Worakijlawan <topgunz.889@gmail.com>
2015-10-22 18:34:47 +0700

    Edit Login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,16 @@
 @extends('backend.layout.auth')
 
 @section('content')
-<body class="login-page" style="background-color: #ecf0f5;">
-	<div class="col-xs-12 col-sm-4 col-sm-offset-4" style="margin-top: 4%;">
+<body class="hold-transition login-page">
+	<div class="login-box" style="margin-top: 4%;">
 		<div class="login-box-body">
-			<div class="row">
+			<div class="row"> 
 				<div class="col-xs-12 text-center login-logo text-light-blue">
-						<img src="{{URL::to('backend_assets/img/logo.png')}}" style="width: 60%; margin-top: 10px;">
+						<img src="{{URL::to('backend_assets/img/logo.png')}}" style="width: 80%; margin-top: 10px;">
 					</div>
 				<!-- -->
 				<div class="col-xs-12 login-box-right" style="position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px;">
-					<p style="text-align: center;"><strong>Sign in to start your session</strong></p>
+					<p class="login-box-msg">Sign in to start your session</p>
 						@if (count($errors) > 0)
 						<div class="alert alert-danger alert-dismissible alert-popup fade in">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -36,15 +36,15 @@
 						</div>
 
 							<div class="row">
-								<div class="col-xs-7">
+								<div class="col-xs-8">
 									<div class="checkbox">
 										<label>
 											<input type="checkbox" name="remember" value="1" >Remember Me
 										</label>
 									</div>
 								</div>
-								<div class="col-xs-5">
-									<button type="submit" class="btn btn-primary btn-block">Sign in</button>
+								<div class="col-xs-4">
+									<button type="submit" class="btn btn-primary btn-block btn-flat">Sign in</button>
 								</div>
 							</div>
 					</form>
