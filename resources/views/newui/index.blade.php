292d6c22976a2bc8c24b50c98520a309ffa954db
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-08-21 19:01:55 +0700

    add link to #intro

--- resources/views/newui/index.blade.php
+++ resources/views/newui/index.blade.php
@@ -71,7 +71,7 @@
                         {{--<button class="btn btn-lg btn-block header-reserve">Reserve</button>--}}
                         <a class="shedul-embed-button-link btn btn-lg btn-block header-reserve" href="https://app.shedul.com/online_bookings/22414/link">Book Now</a>
                     </div>
-                    <div class="arrow-down"></div>
+                    <div class="arrow-down"><a href="#intro"></a></div>
                 </div>
             </header>
         </section>
@@ -316,6 +316,9 @@
                         margin: '25px auto'
                     });
                 }
+            });
+            $('.arrow-down').click(function(event) {
+                $.scrollTo($("#intro"), 750);
             });
             $(function() {
                 $('nav').bind('click', 'ul li a', function(event) {
